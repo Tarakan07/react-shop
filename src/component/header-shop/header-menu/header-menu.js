227c9a49d6b22fcf6bb1726d3de1b8a847f88357
Tarakan07 <boris_galunga@mail.ru>
2023-10-11 22:19:07 +0300

    change path

--- src/component/header-shop/header-menu/header-menu.js
+++ src/component/header-shop/header-menu/header-menu.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const HeaderMenu = () => {
 	return (
 		<div className="header-menu">
-			<Link to="/">
+			<Link to="/react-shop/">
 				<img
 					src="https://pp.userapi.com/c638829/v638829698/66551/OD7He2C9Kl0.jpg"
 					alt=""
@@ -12,16 +12,16 @@ const HeaderMenu = () => {
 			</Link>
 			<ul>
 				<li>
-					<Link to="/products/">Products</Link>
+					<Link to="/react-shop/products/">Products</Link>
 				</li>
 				<li>
-					<Link to="/posts/">Posts</Link>
+					<Link to="/react-shop/posts/">Posts</Link>
 				</li>
 				<li>
-					<Link to="/quotes/">Quotes</Link>
+					<Link to="/react-shop/quotes/">Quotes</Link>
 				</li>
 				<li>
-					<Link to="/cart">Cart</Link>
+					<Link to="/react-shop/cart">Cart</Link>
 				</li>
 			</ul>
 		</div>
